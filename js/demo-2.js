deaff985f52013d08fddcaa5c4cfc0210cd92b77
jessiealbarian <jessie.albarian@alpin.io>
2017-08-15 18:58:06 -0600

    updated content

--- js/demo-2.js
+++ js/demo-2.js
@@ -81,10 +81,10 @@
                 init();
             }
             _this.pos.y -= _this.velocity;
-            _this.alpha -= 0.0005;
+            _this.alpha -= 0.00005;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.scale*10, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(255,255,255,'+ _this.alpha+')';
+            ctx.fillStyle = 'rgba(139,208,232,'+ _this.alpha+')';
             ctx.fill();
         };
     }
